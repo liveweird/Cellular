c36abd320b169e016f016b0106da62419e2d6961
Sebastian Gebski <sebastian@gebski.net>
2016-03-05 22:32:57 +0100

    Trivial visuals

---
+++ Cellular/CellularHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNet.SignalR;
+
+namespace Cellular
+{
+    public class CellularHub : Hub
+    {
+        public void MakeChange()
+        {
+            var random = new Random((int) DateTime.Now.Ticks);
+            Clients.All.addCellChange(random.Next(5), random.Next(5), random.Next(256));
+        }
+    }
+}
